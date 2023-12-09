49d9db9f060a260866b98e6cb329f2451fe49a05
Vasu-Sinojia <sinojiavasu321@gmail.com>
2023-12-10 00:20:56 +0530

    initial_commit
    - project setup

---
+++ android/app/src/main/kotlin/com/example/klimbb_project_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.klimbb_project_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:klimbb_project_task/src/home/views/home.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Home(),
+    ),
+  );
+}
